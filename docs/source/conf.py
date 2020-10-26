440b50594cf9c1024772a75912a7eb96e12a28f4
GertDekkers <Gert.Dekkers@kuleuven.be>
2020-10-26 17:57:07 +0100

    compile of readthedocs not working. Trying to fix issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,10 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = [
-    'numpy', 'numpy.random', 'matplotlib', 'matplotlib.pyplot',
-    'setuptools', 'python-dotenv', 'torch', 'dcase_util', 'soundfile']
+# MOCK_MODULES = [
+#     'numpy', 'numpy.random', 'matplotlib', 'matplotlib.pyplot',
+#     'setuptools', 'python-dotenv', 'torch', 'dcase_util', 'soundfile']
+MOCK_MODULES = ['soundfile']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
