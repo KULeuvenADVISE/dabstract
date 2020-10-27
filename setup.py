d733ed86aa83658996c8b37c29f47bb33b9ee5fd
GertDekkers <Gert.Dekkers@kuleuven.be>
2020-10-27 12:49:29 +0100

    Changed version of sphinx_rtd_theme as it didnt render numpydoc correctly

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="dabstract", # Replace with your own username
     version="0.0.1",
-    author="Gert Dekkers",
+    author="Gert Dekkers, DTAI_ADVISE, KU Leuven",
     author_email="gert.dekkers@gmail.com",
-    description="Database and preprocessing abstractor",
+    description="Database and preprocessing abstraction library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/KULeuvenADVISE/dabstract",
